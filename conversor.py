6e0868ed8bb5c82d167096db4eaa1bcf73308b44
Martin <martindeibe@gmail.com>
2022-02-08 20:30:15 +0100

    Treminada clase conversor

--- conversor.py
+++ conversor.py
@@ -1,6 +1,20 @@
+from monedas import Dolar, Libra, Yen
+
+
 def leer_entero(texto):
     try:
         return int(input(texto))
+    except ValueError:
+        return None
+
+
+def texto_valor():
+    print("Escriba la cantidad: ", end="")
+
+
+def leer_valor():
+    try:
+        return float(input())
     except ValueError:
         return None
 
@@ -17,11 +31,37 @@ CONVERSOR DE MONEDAS
         opcion = leer_entero("Escoja una opcion: ")
 
         if opcion == 1:
-            pass
+            texto_valor()
+            leer_cantidad = leer_valor()
+
+            if isinstance(leer_cantidad, float):
+                moneda = Dolar(leer_cantidad)
+                print(f'La cantidad en euros es {Dolar.cantidadEnEuros(moneda)}')
+            else:
+                print("Introduzca un numero valido")
+                
         elif opcion == 2:
-            pass
+
+            texto_valor()
+            leer_cantidad = leer_valor()
+
+            if isinstance(leer_cantidad, float):
+                moneda = Libra(leer_cantidad)
+                print(f'La cantidad en euros es {Libra.cantidadEnEuros(moneda)}')
+            else:
+                print("Introduzca un numero valido")
+
         elif opcion == 3:
-            pass
+
+            texto_valor()
+            leer_cantidad = leer_valor()
+
+            if isinstance(leer_cantidad, float):
+                moneda = Yen(leer_cantidad)
+                print(f'La cantidad en euros es {Yen.cantidadEnEuros(moneda)}')
+            else:
+                print("Introduzca un numero valido")
+
         elif opcion == 0:
             exit(0)
         else:
